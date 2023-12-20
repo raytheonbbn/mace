12571bde99fd6fb30f967218e461939fde85d466
Bobby Simidchieva <bsimidch@bbn.com>
2023-12-20 13:09:17 -0500

    Updating .sh scripts' headers to run correctly

--- scripts/docker/start_mosquitto.sh
+++ scripts/docker/start_mosquitto.sh
@@ -1,8 +1,8 @@
+#! /bin/bash
+
 #  Approved for public release: distribution is unlimited. PA Case Number AFRL-2023-4617.
 
 #  © 2023 Raytheon BBN Technologies Corp. All rights reserved. Sponsored by the Air Force Research Laboratory (AFRL)  
-
-
 
 #!/bin/bash
 mosquitto -v -c /etc/mosquitto/mosquitto.conf
