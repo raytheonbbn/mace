faa78781583b2bf9acdc999dcb609536015c6502
Brendan Teich <brendan.p.teich@raytheon.com>
2023-12-20 16:53:17 -0500

    Update SSL certs since they expired, add in a util script to regenerate them, and fix a python package issue.

---
+++ config/mosquitto/ssl/copy_crts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ca_certificates/ca.crt ../../../src/analytics_server/config/ca_certificates/ca.crt
+cp ca_certificates/ca.crt ../../../code/stomp/config/ca_certificates/ca.crt
+cp ca_certificates/ca.crt ../../../src/hardware/ca_certificates/ca.crt
